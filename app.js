fd9bbe12bd64fb6af52b19dc4b9d17988571bff1
JÔNATHA <jonatha.mendonca@hotmail.com>
2021-01-28 19:43:32 -0300

    add css

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const express = require("express");
-const app = express();
+/*
+porta const = 3000;
+// Atende arquivos estáticos (precisamos importar um arquivo css) 
+app.use (express.static ('public'))
+// Define uma rota básica 
+app.get ('/', (req, res) => res.send ('Hello World!'));
+
+// Faz com que o aplicativo escute a porta 3000 
+app.listen (port, () => console.log (`Aplicativo escutando a porta $ {port}`));
+
+*/
+const express = require("express");// Carrega o módulo express
+const app = express();// Cria nosso servidor expresso
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser")
 const moment = require('moment')
@@ -16,10 +27,32 @@ app.engine('handlebars', handlebars({
 }))
 app.set('view engine', 'handlebars')
 
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//app.use("/bootstrap", express.static(__dirname+"/node_modules/bootstrap/dist"))
+app.use("/bootstrap", express.static("/node_modules/bootstrap/dist"))
+
+
+//Serves static files (we need it to import a css file)
+app.use(express.static('public'))// Atende arquivos estáticos (precisamos importar um arquivo css) 
+
+//app.use(express.static((__dirname, '/public')));
+
+app.use(express.static('images')); 
+
+
+
+// Define uma rota básica 
+app.get ('/', (req, res) => res.send ('Hello World!'));
+
+
 //Rotas
+
+
+
 app.get('/pagamento', function(req, res){
     Pagamento.findAll({order: [['id', 'DESC']]}).then(function(pagamentos){
         res.render('pagamento', {pagamentos: pagamentos});
