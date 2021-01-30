df5a3f3d1abacfcb84bc9dee4eded766b471e724
JÔNATHA <jonatha.mendonca@hotmail.com>
2021-01-30 15:02:04 -0300

    add host db

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize")
 
 //const sequelize = new Sequelize('celke', 'root', '7z^DbF_r!6Wv[`d+',{
     const sequelize = new Sequelize('heroku_391875727dff55b', 'be45e39a4c81f6', '55fadb18',{
-    host: 'localhost',
+    host: 'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql'
 })
 
