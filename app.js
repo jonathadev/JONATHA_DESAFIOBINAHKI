4ce64d1061d725f398199348eb312f57cf334460
JÔNATHA <jonatha.mendonca@hotmail.com>
2021-01-30 13:59:40 -0300

    deixar pra porta do heroku

--- app.js
+++ app.js
@@ -60,4 +60,7 @@ app.get('/del-pagamento/:id', function(req, res){
     })
 });
 
-app.listen(8080);
+//app.listen(8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
